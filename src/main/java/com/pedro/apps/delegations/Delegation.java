7fb68b2da759c364057070ae986db632aa408b74
pedroGEOGIScoding <pedromartinezduran@gmail.com>
2025-05-09 18:38:48 +0200

    Added Delegation.java

---
+++ src/main/java/com/pedro/apps/delegations/Delegation.java
@@ -0,0 +1,110 @@
+package com.pedro.apps.delegations;
+
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
+
+@DynamoDbBean
+public class Delegation {
+	private String delegationId;
+	private String operation;
+	private String name;
+	private String address;
+	private String city;
+	private float latDelegation;
+	private float lonDelegation;
+	private int availableCarQty;
+	private String phone;
+	private String email;
+
+	@DynamoDbPartitionKey
+	public String getDelegationId() {
+		return delegationId;
+	}
+
+	public void setDelegationId(String delegationId) {
+		this.delegationId = delegationId;
+	}
+
+	@DynamoDbSortKey
+	public String getOperation() {
+		return operation;
+	}
+
+	public void setOperation(String operation) {
+		this.operation = operation;
+	}
+
+	@DynamoDbAttribute("name")
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@DynamoDbAttribute("address")
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@DynamoDbAttribute("city")
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@DynamoDbAttribute("latDelegation")
+	public float getLatDelegation() {
+		return latDelegation;
+	}
+
+	public void setLatDelegation(float latDelegation) {
+		this.latDelegation = latDelegation;
+	}
+
+	@DynamoDbAttribute("lonDelegation")
+	public float getLonDelegation() {
+		return lonDelegation;
+	}
+
+	public void setLonDelegation(float lonDelegation) {
+		this.lonDelegation = lonDelegation;
+	}
+
+	@DynamoDbAttribute("availableCarQty")
+	public int getAvailableCarQty() {
+		return availableCarQty;
+	}
+
+	public void setAvailableCarQty(int availableCarQty) {
+		this.availableCarQty = availableCarQty;
+	}
+
+	@DynamoDbAttribute("phone")
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	@DynamoDbAttribute("email")
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
